0ae9e126fdfd0c3c9af38a62a1aa965d25bcf73c
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2023-09-16 02:17:02 +0300

    + подшаблон

--- catalog/views.py
+++ catalog/views.py
@@ -4,15 +4,19 @@ from catalog.models import Product
 def home(request):
     product_list = Product.objects.all
     context = {
-        'object_list': product_list
+        'object_list': product_list,
+        'title': 'Главная'
     }
     return render(request, 'catalog/home.html', context)
 
 
 def contacts(request):
+    context = {
+        'title': 'Контакты'
+    }
     if request.method == "POST":
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         print(f'{name}, {phone}: {message}')
-    return render(request, 'catalog/contacts.html')
+    return render(request, 'catalog/contacts.html', context)
