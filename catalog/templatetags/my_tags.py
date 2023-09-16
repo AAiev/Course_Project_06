d1f2cba39193ce3a17eb60e6bc2537e704ec38c1
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2023-09-17 02:19:11 +0300

    добавил отображение картинок через шаблонный тег и шаблонный фильтр

---
+++ catalog/templatetags/my_tags.py
@@ -0,0 +1,19 @@
+from django import template
+
+register = template.Library()
+
+@register.filter()
+def media_file(shot_path):
+    if shot_path:
+        return f'/media/{shot_path}'
+    else:
+        return '#'
+
+
+
+@register.tags()
+def media_file(shot_path):
+    if shot_path:
+        return f'/media/{shot_path}'
+    else:
+        return '#'
