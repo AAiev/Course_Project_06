992d282afa6c3c112ed5212fa4261e8b211b48ab
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2023-09-21 02:55:53 +0300

    edit

--- catalog/views.py
+++ catalog/views.py
@@ -23,18 +23,9 @@ def contacts(request):
     return render(request, 'catalog/contacts.html', context)
 
 
-
-def product(request, pk):
-    product_item = Product.objects.get(pk=pk)
-    context = {
-        'object_list':  Product.objects.filter(pk=pk),
-        'title': product_item.name,
-    }
-    return render(request, 'catalog/product.html', context)
-
 class ProductDetailView(DetailView):
     model = Product
-    template_name = 'catalog/home.html'
+    template_name = 'catalog/product.html'
 
     def get_context_data(self, *args, **kwargs):
         context_data = super().get_context_data(*args, **kwargs)
